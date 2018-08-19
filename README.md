418bcae4442b351108eb15d4a8f10357fb521764
Holger Benl <hbenl@evandor.de>
2018-08-19 13:12:38 +0200

    more documentation

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 # VS Code Test Adapter API
 
-This package contains the API that Test Adapters need to implement to work with the
+This package contains the APIs that Test Adapters or Test Controllers need to implement to work with the
 [VS Code Test Explorer](https://github.com/hbenl/vscode-test-explorer).
+
+The API documentation can be found [here](https://github.com/hbenl/vscode-test-adapter-api/blob/master/src/index.ts)
+
+## New API in version 1.0
+
+Version 1.0 of this package contains a new Test Adapter API, which is very similar to the pre-1.0 API
+but requires some changes to Test Adapters. The old API is still supported but it is deprecated, so
+Test Adapters should move to the new API.
+You can still access the old API with an import like this:
+```
+import { TestAdapter } from 'vscode-test-adapter-api/out/legacy';
+```
+To move to the new API, the following changes are necessary:
+* the Test Adapter must offer a new event source `tests` that is used to send events when it starts and finishes
+  loading the test definitions:
+```
+tests: vscode.Event<TestLoadStartedEvent | TestLoadFinishedEvent>;
+```
+* it should start loading the test definitions immediately after being registered with the Test Explorer extension
+  (instead of waiting for a call to the `load()` method)
+* when it detects any changes to the test files or its configuration that make it necessary to reload the test
+  definitions, it should start doing so immediately; the `reload` event source has been removed
+* the loaded test definitions are now sent using the `TestLoadFinishedEvent`, not as the return value of the 
+  `load()` method
+* it must send a `TestRunStartedEvent` and a `TestRunFinishedEvent` at the beginning and end of a test run, repectively.
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,149 @@
 import * as vscode from 'vscode';
 import { TestAdapter as LegacyTestAdapter } from './legacy';
 
+/**
+ * The ID of the Test Explorer extension. Use it to get the `TestHub` API like this:
+ * ```
+   const testHub = vscode.extensions.getExtension<TestHub>(testExplorerExtensionId).exports; ```
+ * Don't forget to add an `extensionDependencies` entry to your `package.json`:
+ * ```
+   "extensionDependencies": [ "hbenl.vscode-test-explorer" ] ```
+ */
 export const testExplorerExtensionId = 'hbenl.vscode-test-explorer';
 
+/**
+ * This is the interface offered by the Test Explorer extension for registering
+ * and unregistering Test Adapters and Test Controllers.
+ */
 export interface TestHub {
-	registerAdapter(adapter: LegacyTestAdapter): void;
-	unregisterAdapter(adapter: LegacyTestAdapter): void;
 	registerTestAdapter(adapter: TestAdapter): void;
 	unregisterTestAdapter(adapter: TestAdapter): void;
 	registerTestController(controller: TestController): void;
 	unregisterTestController(controller: TestController): void;
+
+	/**
+	 * @deprecated this is for adapters using the pre-1.0 API
+	 */
+	registerAdapter(adapter: LegacyTestAdapter): void;
+	/**
+	 * @deprecated this is for adapters using the pre-1.0 API
+	 */
+	unregisterAdapter(adapter: LegacyTestAdapter): void;
 }
 
+/**
+ * This is the interface that must be implemented by Test Adapters.
+ */
 export interface TestAdapter {
+
+	/**
+	 * The workspace folder that this test adapter is associated with (if any).
+	 * There is usually one test adapter per workspace folder and testing framework.
+	 */
 	workspaceFolder?: vscode.WorkspaceFolder;
+
+	/**
+	 * Start loading the definitions of tests and test suites.
+	 * Note that this method is only used to give the user a chance to manually request a reload.
+	 * The Test Adapter should load the test definitions immediately after being registered with
+	 * the Test Explorer extension and should automatically reload them if necessary (due to changes
+	 * to the test files or the adapter's configuration).
+	 * @returns A promise that is resolved when the adapter finished loading the test definitions.
+	 */
 	load(): Promise<void>;
+
+	/**
+	 * Run the specified tests.
+	 * @returns A promise that is resolved when the test run is completed.
+	 */
 	run(tests: TestSuiteInfo | TestInfo): Promise<void>;
+
+	/**
+	 * Run the specified tests in the debugger.
+	 * @returns A promise that is resolved when the test run is completed.
+	 */
 	debug(tests: TestSuiteInfo | TestInfo): Promise<void>;
+
+	/**
+	 * Stop the current test run.
+	 */
 	cancel(): void;
+
+	/**
+	 * This event is used by the adapter to inform the Test Explorer (and other Test Controllers)
+	 * that it started or finished loading the test definitions.
+	 */
 	readonly tests: vscode.Event<TestLoadStartedEvent | TestLoadFinishedEvent>;
+
+	/**
+	 * This event is used by the adapter during a test run to inform the Test Explorer
+	 * (and other Test Controllers) about a test run and tests and suites being started or completed.
+	 * For example, if there is one test suite with ID `suite1` containing one test with ID `test1`,
+	 * a successful test run would emit the following events:
+	 * ```
+	 * { type: 'started' }
+	 * { type: 'suite', suite: 'suite1', state: 'running' }
+	 * { type: 'test', test: 'test1', state: 'running' }
+	 * { type: 'test', test: 'test1', state: 'passed' }
+	 * { type: 'suite', suite: 'suite1', state: 'completed' }
+	 * { type: 'finished' } ```
+	 */
 	readonly testStates: vscode.Event<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent>;
+
+	/**
+	 * This event can be used by the adapter to trigger a test run for all tests that have
+	 * been set to "autorun" in the Test Explorer.
+	 */
 	readonly autorun?: vscode.Event<void>;
 }
 
+/**
+ * This is the interface that must be implemented by Test Controllers
+ */
 export interface TestController {
+
+	/**
+	 * Register the given Test Adapter. The Test Controller should subscribe to the `adapter.tests`
+	 * event source immediately in order to receive the test definitions.
+	 */
 	registerTestAdapter(adapter: TestAdapter): void;
+
 	unregisterTestAdapter(adapter: TestAdapter): void;
 }
 
+/**
+ * This event is sent by a Test Adapter when it starts loading the test definitions.
+ */
 export interface TestLoadStartedEvent {
 	type: 'started';
 }
 
+/**
+ * This event is sent by a Test Adapter when it finished loading the test definitions.
+ */
 export interface TestLoadFinishedEvent {
+
 	type: 'finished';
+
+	/** The test definitions that have just been loaded */
 	suite: TestSuiteInfo | undefined;
+}
+
+/**
+ * This event is sent by a Test Adapter when it starts a test run.
+ */
+export interface TestRunStartedEvent {
+	type: 'started';
+
+	/** The test(s) that will be run */
+	tests: TestSuiteInfo | TestInfo;	
+}
+
+/**
+ * This event is sent by a Test Adapter when it finished a test run.
+ */
+export interface TestRunFinishedEvent {
+	type: 'finished';
 }
 
 /**
@@ -49,7 +158,11 @@ export interface TestSuiteInfo {
 	/** The label to be displayed by the Test Explorer for this suite. */
 	label: string;
 
-	/** The absolute path of the file containing this suite (if known). */
+	/**
+	 * The file containing this suite (if known).
+	 * This can either be an absolute path (if it is a local file) or a URI.
+	 * Note that this should never contain a `file://` URI.
+	 */
 	file?: string;
 
 	/** The line within the specified file where the suite definition starts (if known). */
@@ -70,7 +183,11 @@ export interface TestInfo {
 	/** The label to be displayed by the Test Explorer for this test. */
 	label: string;
 
-	/** The absolute path of the file containing this test (if known). */
+	/**
+	 * The file containing this test (if known).
+	 * This can either be an absolute path (if it is a local file) or a URI.
+	 * Note that this should never contain a `file://` URI.
+	 */
 	file?: string;
 
 	/** The line within the specified file where the test definition starts (if known). */
@@ -87,7 +204,8 @@ export interface TestSuiteEvent {
 
 	type: 'suite';
 
-	/** The suite that is being started or completed. This field usually contains the ID of the
+	/**
+	 * The suite that is being started or completed. This field usually contains the ID of the
 	 * suite, but it may also contain the full information about a suite that is started if that
 	 * suite had not been sent to the Test Explorer yet.
 	 */
@@ -128,13 +246,4 @@ export interface TestEvent {
 export interface TestDecoration {
 	line: number;
 	message: string;
-}
-
-export interface TestRunStartedEvent {
-	type: 'started';
-	tests: TestSuiteInfo | TestInfo;	
-}
-
-export interface TestRunFinishedEvent {
-	type: 'finished';
 }
